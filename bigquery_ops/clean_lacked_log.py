c2acd86adb3ce922a8c56450c06e8500ed7492bf
hase-ryo <26161933+hase-ryo@users.noreply.github.com>
2020-09-12 03:35:59 +0900

    delete lacked chatlog

---
+++ bigquery_ops/clean_lacked_log.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+import platform
+import json
+import sys
+import os
+from google.cloud import bigquery
+from google.oauth2 import service_account
+from gcs_wrapper import gcs_wrapper
+
+if platform.system() == 'Darwin':
+    # run locally
+    load_dotenv('.env')
+
+project_id = os.environ.get("PROJECT_ID")
+bucket_name = os.environ.get("GCS_BUCKET_NAME")
+
+def get_bq_client():
+    if platform.system() == 'Linux':
+        # run at cloud
+        client = bigquery.Client()
+    elif platform.system() == 'Darwin':
+        # run locally
+        credentials = service_account.Credentials.from_service_account_file(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"), scopes=["https://www.googleapis.com/auth/cloud-platform"],)
+        client = bigquery.Client(credentials=credentials, project=project_id)
+
+    return(client)
+
+
+def detect_lacked_log():
+    with open('./detect_lacked_log.sql') as f:
+        query_str = f.read()
+
+    query_str = query_str.format(project_id=project_id)
+    bq = get_bq_client()
+    result = bq.query(query_str)
+    obj_list = []
+    for row in result:
+        file_path = row['channel_id'] + '/' + row['video_id'] + '/'
+        if gcs_wrapper.check_gcs_file_exists(bucket_name, file_path):
+            objects = gcs_wrapper.get_gcs_files(bucket_name, file_path)
+            for obj in objects:
+                obj_list.append(obj)
+
+    return(obj_list)
+
+if __name__ == '__main__':
+    obj_list = detect_lacked_log()
+    for obj in obj_list:
+        gcs_wrapper.delete_gcs_file(bucket_name, obj)
+
---
+++ bigquery_ops/detect_lacked_log.sql
@@ -0,0 +1,29 @@
+with C as (
+SELECT 
+  video_id
+  ,min(timestampUsec) as min_time
+  ,max(timestampUsec) as max_time
+FROM `{project_id}.livechatlog.chatlog`
+group by
+  video_id
+), CT as (
+  select
+    video_id
+    ,time(timestamp_micros(cast(max_time - min_time as INT64))) as chatlog_time
+  from C
+)
+SELECT 
+  name
+  ,channel_id
+  ,title
+  ,V.video_id
+  ,time_duration
+  ,chatlog_time
+  ,time_diff(time_duration, chatlog_time, second) as diff_time
+FROM `{project_id}.Vtuber.v_videos` V
+left outer join CT
+  on V.video_id = CT.video_id 
+where
+  time_diff(time_duration, chatlog_time, second) > 600
+limit 3
+;
